e36c3d6041a4dd506e60246f0b53d41156e416b3
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-24 16:43:52 +0100

    Update Main.java

--- 03._Advanced_data_structures/vista/Main.java
+++ 03._Advanced_data_structures/vista/Main.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vista;
 
 import static Controlador.Controlador.actualizarProducto;
-import static Controlador.Controlador.agregarProducto1;
+import static Controlador.Controlador.agregarProducto;
 import static Controlador.Controlador.eliminarProducto;
+import Controlador.EscepcionesMias;
 import java.util.ArrayList;
 import java.util.HashMap;
 import static miLibreria.ValidDat.enteroEntreDosValores;
@@ -14,12 +12,15 @@ import modelos.Producto;
 
 /**
  *
- * @author eduardolucasmunozdelucas
+ * @author edulumulu
  */
 public class Main {
 
     public static HashMap<String, Producto> listaProductos = new HashMap<>();
 
+    /**
+    *Imprime por pantalla un menú con las opciones
+    */
     public static void menu() {
         System.out.println("""
                            -------- MENU --------
@@ -30,36 +31,51 @@ public class Main {
                            4. Salir
                            ----------------------""");
     }
-    
-    public static void recorrerlista(){
-        System.out.println(" ");
-         System.out.println("--- LISTA DE PRODUCTOS ACTUALES ---");
-                int contador = 1;
-                for (String k : listaProductos.keySet()) {
-                    System.out.println("Producto " + contador + " --->  Código: " + k + " " + listaProductos.get(k));
-                    contador++;
 
-                }
-                System.out.println(" ");
+    /**
+     * Recorre el mapa lista de productos
+     */
+    public static void recorrerlista() {
+        System.out.println(" ");
+        System.out.println("--- LISTA DE PRODUCTOS ACTUALES ---");
+        int contador = 1;
+        if (!listaProductos.isEmpty()) {
+            for (String k : listaProductos.keySet()) {
+                System.out.println("Producto " + contador + " --->  Código: " + k + " " + listaProductos.get(k));
+                contador++;
+            }
+        } else {
+            System.out.println("Ninguno");
+        }
+
+        System.out.println(" ");
     }
-    
-    public static void rellenarListaPredeterminada(){
-        ArrayList <String> mat= new ArrayList<>();
+
+    /**
+     * Rellena de forma predeterminada cuatro ocurrencias en el mapa lista de Productos
+     */
+    public static void rellenarListaPredeterminada() {
+        ArrayList<String> mat = new ArrayList<>();
         mat.add("Algodon");
         mat.add("Piel");
-        listaProductos.put("2B", new Producto("casa",mat , 500000 , 2));
-        listaProductos.put("1A", new Producto("coche",mat , 20000.7 , 14));
-        listaProductos.put("8Z", new Producto("falda",mat , 10.9 , 100));
-        listaProductos.put("3L", new Producto("valda",mat , 25.7 , 5));
+        listaProductos.put("casa", new Producto("casa", mat, 500000, 2));
+        listaProductos.put("coche", new Producto("coche", mat, 20000.7, 14));
+        listaProductos.put("falda", new Producto("falda", mat, 10.9, 100));
+        listaProductos.put("valda", new Producto("valda", mat, 25.7, 5));
     }
 
-    public static void main(String[] args) {
+    /**
+     * Primero muestra los productos insertados, luego t permite elegir una opción y llama alos distintos metodos con un switch. 
+     * Mientras no se salga con una opcíon se mantiene le bucle en el que muestra el menú para que elija una opción
+     * @param args
+     * @throws EscepcionesMias 
+     */
+    public static void main(String[] args) throws EscepcionesMias {
         boolean ok = true;
         int opt;
-        
+
         recorrerlista();
-        
-        
+
         do {
             menu();
             System.out.print("Inserta la opción deseada: ");
@@ -71,7 +87,7 @@ public class Main {
                     recorrerlista();
                     break;
                 case 1:
-                    agregarProducto1();
+                    agregarProducto();
 
                     break;
                 case 2:
