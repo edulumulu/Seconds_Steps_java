0e0fdfdff729be0f73dc245b4deada185653a4f7
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-24 16:38:18 +0100

    Add files via upload

---
+++ 03._Advanced_data_structures/Controlador/EscepcionesMias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+/**
+ * La verdad es que con esto tengo dudas, en el tema 13 lo he visto mas claro, pero aquí finalmente no lo he utilizado parcialmente
+ * @author eduardolucasmunozdelucas
+ */
+public class EscepcionesMias extends Exception{
+    
+    public EscepcionesMias(String texto) {
+        super(texto);
+    }
+}
